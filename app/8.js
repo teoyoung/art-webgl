a3dec58f7b46e090223c2cb0c01bb1cef280beb2
farsash2007 <farsash2007@gmail.com>
2019-01-31 16:28:01 +0300

    heh

--- app/8.js
+++ app/8.js
@@ -3,76 +3,59 @@ stats.domElement.style.position = 'absolute';
 stats.domElement.style.top = '0';
 document.body.appendChild(stats.domElement);
 
-camera.position.z = 200;
+camera = new THREE.OrthographicCamera( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, 1, 100000 );
+camera.position.z = 1000;
 scene.background = new THREE.Color( 0xffffff );
 
 
 // 
-let edges = new THREE.EdgesGeometry( new THREE.BoxBufferGeometry( 30, 30, 30 ) );
-
+let edges = new THREE.EdgesGeometry( new THREE.BoxBufferGeometry( 2, 2, 2 ) );
 
 
 let gC = new THREE.Group();
 
-for( var j = 0; j < 360*Math.PI/180; j += 30*Math.PI/180 ) {
+let time_s = 0;
+
+let tss = 0;
+
+for( var j = 0; j < 1800*Math.PI/180; j += 20*Math.PI/180 ) {
+
+    
+
+    if(time_s < 1){
+        time_s += 0.1;
+    } else {
+        time_s = 0;
+    }
+
+
     let gB = new THREE.Group();
     let line = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0x000000 } ) );
-    line.position.set( Math.cos( j ) * 100, Math.sin( j ) * 100,  0);
-    line.rotation.z = j ;
+    line.position.set( Math.cos( j ) * 100 * (j*0.2), Math.sin( j ) * 100 * (j*0.2),  0);
+    let sc = 1 * j;
+    line.localpos = 1;
+    let sv = line.position;
+    line.scale.set(sc, sc,sc);
+    line.rotation.z += j ;
+    line.rotateY(tss);
+    line.rotateX(tss);
     line.updateMatrix();
-
-    console.log("fds");
     //line.rotateOnAxis(j);
 
     gB.add( line );
     gC.add( gB );
+
+
+    tss += 0.1;
 }
 
 
 
 scene.add(gC);
 
-
-var rotObjectMatrix;
-function rotateAroundObjectAxis(object, axis, radians) {
-    rotObjectMatrix = new THREE.Matrix4();
-    rotObjectMatrix.makeRotationAxis(axis.normalize(), radians);
-
-    // old code for Three.JS pre r54:
-    // object.matrix.multiplySelf(rotObjectMatrix);      // post-multiply
-    // new code for Three.JS r55+:
-    object.matrix.multiply(rotObjectMatrix);
-
-    // old code for Three.js pre r49:
-    // object.rotation.getRotationFromMatrix(object.matrix, object.scale);
-    // old code for Three.js r50-r58:
-    // object.rotation.setEulerFromRotationMatrix(object.matrix);
-    // new code for Three.js r59+:
-    object.rotation.setFromRotationMatrix(object.matrix);
+function intAnSoc( t ){
+    return Math.pow(Math.abs(Math.sin(t)), 0.5)
 }
-
-var rotWorldMatrix;
-// Rotate an object around an arbitrary axis in world space       
-function rotateAroundWorldAxis(object, axis, radians) {
-    rotWorldMatrix = new THREE.Matrix4();
-    rotWorldMatrix.makeRotationAxis(axis.normalize(), radians);
-
-    // old code for Three.JS pre r54:
-    //  rotWorldMatrix.multiply(object.matrix);
-    // new code for Three.JS r55+:
-    rotWorldMatrix.multiply(object.matrix);                // pre-multiply
-
-    object.matrix = rotWorldMatrix;
-
-    // old code for Three.js pre r49:
-    // object.rotation.getRotationFromMatrix(object.matrix, object.scale);
-    // old code for Three.js pre r59:
-    // object.rotation.setEulerFromRotationMatrix(object.matrix);
-    // code for r59+:
-    object.rotation.setFromRotationMatrix(object.matrix);
-}
-
-
 
 // Animation
 function InterAnim(s, f, t ){
@@ -80,12 +63,15 @@ function InterAnim(s, f, t ){
 }
 
 let time = 0;
+let k = 0;
 
 function animate() {
 
     if (time < 1){
-        time += 0.01;
+        time += 0.0001;
     }
+
+    k += 0.01;
 
     requestAnimationFrame( animate );
     renderer.render( scene, camera );
@@ -93,11 +79,29 @@ function animate() {
 
     gC.children.forEach(function( e ) {
         /*
-        e.children[0].rotation.x += 0.01;
+        
         e.children[0].rotation.y += 0.01;
         */
         //e.children[0].position.x += 0.1;
-        //e.children[0].translateX(0.1);
+
+        /*
+
+        if(e.children[0].localpos < 20){
+            e.children[0].localpos += 0.05;
+            e.children[0].translateX(1);
+            e.children[0].rotateX(0.01);
+            e.children[0].scale.set(e.children[0].localpos, e.children[0].localpos, e.children[0].localpos)
+        } else {
+            e.children[0].localpos = 1;
+            console.log(e.children[0].position.x, e.children[0].savepos.x);
+        }
+        */
+
+        
+        
+        
+
+        //console.log(e.children[0].position.x);
 
         //var v1 = new THREE.Vector3( 0.6, 0, 0 );
         //e.children[0].translateOnAxis(v1, 0.4);
@@ -106,7 +110,9 @@ function animate() {
         
     });
 
-    //gC.rotation.z += 0.01;
+    //gC.rotation.z -= 18.5;
+    gC.rotation.z -= 0.7 / 2;
+    //console.log(k);
     
 
 }
