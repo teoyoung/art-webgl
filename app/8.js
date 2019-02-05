1028e143e277770f971a593856d1516265772dd4
farsash <farsash2007@gmail.com>
2019-02-05 22:23:59 +0300

    cube fix

--- app/8.js
+++ app/8.js
@@ -9,53 +9,9 @@ camera = new THREE.OrthographicCamera( window.innerWidth / - zoom, window.innerW
 camera.position.z = 1000;
 scene.background = new THREE.Color( 0xffffff );
 
-
-
-function DrawEdgeFunction( lenght, width, size ){
-
-    let ln = lenght / 2;   
-
-    let elements = new THREE.Object3D();
-
-    let drawEdge = [
-        { x:0, y:-ln, z:-ln, rx:0, ry:0, rz:0 }, { x:0, y:-ln, z:ln, rx:0, ry:0, rz:0 },
-        { x:0, y:ln, z:-ln, rx:0, ry:0, rz:0 }, { x:0, y:ln, z:ln, rx:0, ry:0, rz:0 },
-        { x:ln, y:0, z:-ln, rx:0, ry:0, rz:90, lenght: true }, { x:-ln, y:0, z:-ln, rx:0, ry:0, rz:90, lenght: true },
-        { x:ln, y:0, z:ln, rx:0, ry:0, rz:90, lenght: true }, { x:-ln, y:0, z:ln, rx:0, ry:0, rz:90, lenght: true },
-        { x:ln, y:-ln, z:0, rx:0, ry:90, rz:0, lenght: true }, { x:ln, y:ln, z:0, rx:0, ry:90, rz:0, lenght: true },
-        { x:-ln, y:-ln, z:0, rx:0, ry:90, rz:0, lenght: true }, { x:-ln, y:ln, z:0, rx:0, ry:90, rz:0, lenght: true }
-    ];    
-
-    drawEdge.forEach(function( e ) {
-
-        let lenghtEdge = lenght;
-
-        if(e.lenght){
-            lenghtEdge = lenght * 0.015 + lenght;
-        }        
-
-        var cube = new THREE.Mesh( new THREE.BoxBufferGeometry( lenghtEdge, width, width ), new THREE.MeshBasicMaterial( {color: 0x000000} ) );
-        cube.position.set(e.x, e.y, e.z);
-        cube.rotation.set(e.rx *Math.PI/180, e.ry*Math.PI/180, e.rz*Math.PI/180);
-        elements.add( cube );
-
-    });
-
-    elements.scale.set(size, size, size);
-
-    return elements;
-
-}
-
-
-
-
 let edges = new THREE.EdgesGeometry( new THREE.BoxBufferGeometry( 2, 2, 2 ) );
 
-
 let gC = new THREE.Group();
-
-let time_s = 0;
 
 let tss = 0.5;
 let scale_ = 0;
@@ -74,26 +30,14 @@ for( var j = 0; j < 2800*Math.PI/180; j += 20*Math.PI/180 ) {
         }
         
     }       
-
-    if(time_s < 1){
-        time_s += 0.01;
-    } else {
-        time_s = 0;
-    }
-
-
-
     
     let line = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0x000000 } ) );
     line.position.set( Math.cos( j ) * 100 * (j*0.2), Math.sin( j ) * 100 * (j*0.2),  0);
-    let sc = 1 * j;
-    line.localpos = 1;
     let sv = line.position;
     line.scale.set(scale_, scale_,scale_);
     line.rotation.z += j ;
     line.rotateY(tss);
     line.rotateX(tss);
-    //line.rotateOnAxis(j);
 
     gB.add( line );
     gC.add( gB );
@@ -102,21 +46,7 @@ for( var j = 0; j < 2800*Math.PI/180; j += 20*Math.PI/180 ) {
 
 }
 
-
-
 scene.add(gC);
-
-function intAnSoc( t ){
-    return Math.pow(Math.abs(Math.sin(t)), 0.5)
-}
-
-// Animation
-function InterAnim(s, f, t ){
-    return (1-t)*s + t * f;
-}
-
-let time = 0;
-let k = 0;
 
 function animate() {
 
@@ -124,11 +54,7 @@ function animate() {
     renderer.render( scene, camera );
     stats.update();
 
-
-    //gC.rotation.z -= 18.5;
     gC.rotation.z -= 0.7 /2;
-    //console.log(k);
-
 
 }
 
