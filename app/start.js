5bd981f54303a459d78c931ed126dae1233674d0
Farsash <farsash2007@gmail.com>
2019-04-07 23:21:46 +0300

    greed complt

--- app/start.js
+++ app/start.js
@@ -2,21 +2,20 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 1.0, 20000 );
 
 
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-camera.position.z = 500;
+let contril_sis = false;
+var controls = new THREE.OrbitControls( camera );
+if (contril_sis){
+} else {
+    camera.position.y = 5;
+    camera.position.z = 5;
+    camera.lookAt(new THREE.Vector3(0,0,0));
+}
 
 //
 
 let t_loader = new THREE.TextureLoader();
 let g_loader = new THREE.ObjectLoader();
-
-window.addEventListener( 'resize', onWindowResize, false );
-
-
-function onWindowResize() {
-    camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-}
